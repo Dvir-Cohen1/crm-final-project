a7fdeaa4190056902bd9375490735d14226d3335
Dvir Cohen <dvir906@gmail.com>
2023-04-29 23:20:04 +0300

    test middleware

--- server/src/middlewares/authentication.middleware.ts
+++ server/src/middlewares/authentication.middleware.ts
@@ -16,17 +16,17 @@ const authJwtTokenVerify = (
       ? req.headers["access-token"][0]
       : req.headers["access-token"];
 
-    if (!token) return next(new UnauthorizeError());
-    const decodedToken = verifyAccessToken(token);
-    // console.log(decodedToken.userId);
-    if (typeof decodedToken === "string") {
-      console.log(decodedToken)
-      req.userId = decodedToken;
-    } else {
-      // console.log(decodedToken.userId)
-      req.userId = decodedToken.userId;
-      // req.userId = decodedToken.sub as string;
-    }
+    // if (!token) return next(new UnauthorizeError());
+    // const decodedToken = verifyAccessToken(token);
+    // // console.log(decodedToken.userId);
+    // if (typeof decodedToken === "string") {
+    //   console.log(decodedToken)
+    //   req.userId = decodedToken;
+    // } else {
+    //   // console.log(decodedToken.userId)
+    //   req.userId = decodedToken.userId;
+    //   // req.userId = decodedToken.sub as string;
+    // }
     // console.log(req.userId)
     next();
   } catch (error) {
