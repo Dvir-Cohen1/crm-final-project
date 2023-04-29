f5760b02639f2087775d51e7486e8e837c4cba4e
Dvir Cohen <dvir906@gmail.com>
2023-04-29 22:13:49 +0300

    edit env

--- server/src/config/initialConnection.ts
+++ server/src/config/initialConnection.ts
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-// const uri = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.p6w2ece.mongodb.net/?retryWrites=true&w=majority`;
-
-const uri = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.ed4xrrh.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `${process.env.MONGO_ATLAS_URI}`;
 
 const initialMongoConnection = async () => {
   mongoose.set("strictQuery", false);
   return new Promise((resolve, reject) => {
     resolve(mongoose.connect(uri));
   });
+
 };
 
 export default initialMongoConnection;
